94454a82187f6671e59d07477888272023e1755a
Fazil S <fazil.s@experionglobal.local>
2023-11-04 12:05:27 +0530

    fix:player object creation and scoreupdate using js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+
+
+let player1 = {
+    playerName :"",
+    playerScore:"5",
+
+}
+
+let scoreUpdate = document.getElementById("current--0").textContent=player1.playerScore;
+
+
+
