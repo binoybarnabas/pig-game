df39e66db4cbd1724b27fab09298a998c1f1a0f3
Anish S <anish.s@experionglobal.local>
2023-11-04 12:41:21 +0530

    fix: player object fixed with nested object

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-'use strict';
+let players = {
+    player1:{
+      playerName:"",
+      playerScore:0,
+      playerCurrentScore:0
+    },
+    player2:{
+      playerName:"",
+      playerScore:0,
+      playerCurrentScore:0
+    }
+  };
+  
+
 
 function rollDice() {
   let rolls;
   rolls = Math.floor(Math.random() * 6) + 1;
 
 }
-let rolls = 3;
+let rolls = 6;
 
 const diceImage = document.getElementById("image");
 diceImage.src = `dice-${rolls}.png`;
 
-let player1 = {
-    playerName :"",
-    playerScore:"5",
 
-}
 
 let scoreUpdate = document.getElementById("current--0").textContent=player1.playerScore;
 
