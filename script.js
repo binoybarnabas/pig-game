f88c82f02199ba6ada33b9fb7a8aca6623e06874
Anish S <anish.s@experionglobal.local>
2023-11-04 14:54:27 +0530

    feat:- roll dice image change fixed

--- script.js
+++ script.js
@@ -1,31 +1,22 @@
 let players = {
-    player1:{
-      playerName:"",
-      playerScore:0,
-      playerCurrentScore:0
-    },
-    player2:{
-      playerName:"",
-      playerScore:0,
-      playerCurrentScore:0
-    }
-  };
-  
-
+  player1: {
+    playerName: '',
+    playerScore: 0,
+    playerCurrentScore: 0,
+  },
+  player2: {
+    playerName: '',
+    playerScore: 0,
+    playerCurrentScore: 0,
+  },
+};
 
 function rollDice() {
   let rolls;
   rolls = Math.floor(Math.random() * 6) + 1;
-
+  const diceImage = document.getElementById('image');
+  diceImage.src = `dice-${rolls}.png`;
 }
-let rolls = 6;
 
-const diceImage = document.getElementById("image");
-diceImage.src = `dice-${rolls}.png`;
-
-
-
-let scoreUpdate = document.getElementById("current--0").textContent=player1.playerScore;
-
-
-
+let scoreUpdate = (document.getElementById('current--0').textContent =
+  player1.playerScore);
