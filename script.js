ef8ab8c3abc84e0b458ed03c9cd678e7d2e822c8
Binoy Barnabas <binoy.barnabas@experionglobal.local>
2023-11-05 15:45:08 +0530

    fix:- script.js code refactored

--- script.js
+++ script.js
@@ -1,150 +1,94 @@
 let players = {
-  player1: {
-    playerName: '',
-    playerScore: 0,
-    playerCurrentScore: 0,
-    playerActive: true,
+  player1:{
+    playerScore:0,
+    playerCurrentScore:0,
+    player1Active:true
   },
-  player2: {
-    playerName: '',
-    playerScore: 0,
-    playerCurrentScore: 0,
-    playerActive: false,
-  },
-};
-
-document.querySelector('.player--0').classList.add('player--active');
-
-let rollsSum = 0;
-
-let mainScore1 = document.getElementById('score--0');
-let mainScore2 = document.getElementById('score--1');
-
-
-function rollDice() {
-  if (players.player1.playerActive == true) {
-    document.querySelector('.player--0').classList.add('player--active');
-    document.querySelector('.player--1').classList.remove('player--active');
-
-    rolls = Math.floor(Math.random() * 6) + 1;
-
-    const diceImage = document.getElementById('image');
-    diceImage.src = `dice-${rolls}.png`;
-
-    if (rolls == 1) {
-      players.player1.playerScore = 0;
-      players.player1.playerCurrentScore=0;
-      document.getElementById('score--0').textContent = players.player1.playerScore;
-      document.getElementById('current--0').textContent = players.player1.playerCurrentScore;
-      rollsSum = 0;
-      players.player1.playerActive = false;
-      players.player2.playerActive = true;
-    } else {
-      rollsSum += rolls;
-      let player1 = {
-        playerName: '',
-        playerScore: rollsSum,
-      };
-
-      document.getElementById('current--0').textContent = player1.playerScore;
-    }
-  } else {
-    document.querySelector('.player--1').classList.add('player--active');
-    document.querySelector('.player--0').classList.remove('player--active');
-
-    rolls = Math.floor(Math.random() * 6) + 1;
-
-    const diceImage = document.getElementById('image');
-    diceImage.src = `dice-${rolls}.png`;
-    if (rolls == 1) {
-      players.player2.playerScore = 0;
-      players.player2.playerCurrentScore = 0;
-      document.getElementById('score--1').textContent =  players.player2.playerScore;
-      document.getElementById('current--1').textContent = players.player2.playerCurrentScore;
-      rollsSum = 0;
-
-      players.player2.playerActive = false;
-      players.player1.playerActive = true;
-    } else {
-      rollsSum += rolls;
-      let player2 = {
-        playerName: '',
-        playerScore: rollsSum,
-      };
-
-      document.getElementById('current--1').textContent = player2.playerScore;
-    }
+  player2:{
+    playerScore:0,
+    playerCurrentScore:0,
+    player2Active:false
   }
-  return rolls;
+}
+let rollDiceDigit;
+
+const randomNumber = ()=>{
+  const rollDiceDigit = Math.ceil(Math.random()*6);
+  return rollDiceDigit;
 }
 
-const newGame = () => {
-
-  players.player1.playerScore = 0;
-  players.player2.playerScore = 0;
-
-  document.getElementById('current--0').textContent =
-    players.player1.playerScore;
-  document.getElementById('current--1').textContent =
-    players.player2.playerScore;
-
-  mainScore1.textContent = 0;
-  mainScore2.textContent = 0;
-  rollsSum = 0;
-};
-
-const hold = () => {
-  // players.player1.playerScore = 0;
-  if (players.player1.playerActive == true) {
-    document.querySelector('.player--1').classList.add('player--active');
-    document.querySelector('.player--0').classList.remove('player--active');
-
-    players.player1.playerCurrentScore += rollsSum;
-    mainScore1.textContent = players.player1.playerCurrentScore;
-
-    if (players.player1.playerCurrentScore >= 100) {
-      player = document.querySelector('.player--0');
-      player.classList.add('player--winner');
-    }
-
-    players.player1.playerActive = false;
-    players.player2.playerActive = true;
-
-    players.player1.playerScore = 0;
-
-    document.getElementById('current--0').textContent =
-      players.player1.playerScore;
-
-    rollsSum = 0;
-  } else {
+const rollDice=()=>{
+  rollDiceDigit = randomNumber();
+  const image = document.getElementById('image');
+  image.src = `dice-${rollDiceDigit}.png`;
+  if(players.player1.player1Active == true && players.player2.player2Active == false){
     document.querySelector('.player--0').classList.add('player--active');
     document.querySelector('.player--1').classList.remove('player--active');
-
-    players.player2.playerCurrentScore += rollsSum;
-    mainScore2.textContent = players.player2.playerCurrentScore;
-
-    if (players.player1.playerCurrentScore >= 100) {
-      player = document.querySelector('.player--1');
-      player.classList.add('player--winner');
-      
+    players.player1.playerCurrentScore  += rollDiceDigit;
+    document.getElementById('current--0').innerHTML = players.player1.playerCurrentScore;
+    if(rollDiceDigit == 1){
+      players.player1.playerScore = 0;
+      players.player1.playerCurrentScore = 0;
+      document.getElementById('score--0').innerHTML = players.player1.playerScore;
+      document.getElementById('current--0').innerHTML = players.player1.playerCurrentScore;
+      players.player1.player1Active=false;
+      players.player2.player2Active=true;
     }
-
-    players.player2.playerActive = false;
-    players.player1.playerActive = true;
-
-    players.player2.playerScore = 0;
-    document.getElementById('current--1').textContent =
-      players.player2.playerScore;
-
-    rollsSum = 0;
+  } 
+  else{
+    document.querySelector('.player--0').classList.remove('player--active');
+    document.querySelector('.player--1').classList.add('player--active');
+    players.player2.playerCurrentScore  += rollDiceDigit;
+    document.getElementById('current--1').innerHTML = players.player2.playerCurrentScore;
+    if(rollDiceDigit == 1){
+      players.player2.playerScore = 0;  
+      players.player2.playerCurrentScore = 0;
+      document.getElementById('score--1').innerHTML = players.player2.playerScore;
+      document.getElementById('current--1').innerHTML = players.player2.playerCurrentScore;
+      players.player1.player1Active=true;
+      players.player2.player2Active=false;
+    }
   }
-  scoreUpdate = document.getElementById('current--0').textContent = 0;
-  mainScore.textContent = 0;
-};
 
+}
 
+const hold = () =>{
+if(players.player1.player1Active=true && players.player2.player2Active == false){
+  document.querySelector('.player--0').classList.remove('player--active');
+  document.querySelector('.player--1').classList.add('player--active');
+  players.player1.playerScore = players.player1.playerScore + players.player1.playerCurrentScore;
+  players.player1.playerCurrentScore = 0;
+  document.getElementById('current--0').innerHTML = players.player1.playerCurrentScore;
+  document.getElementById('score--0').innerHTML = players.player1.playerScore;
+  players.player1.player1Active=false;
+  players.player2.player2Active=true;
 
+}
+else{
+  document.querySelector('.player--0').classList.add('player--active');
+  document.querySelector('.player--1').classList.remove('player--active');
+  players.player2.playerScore = players.player2.playerScore+players.player2.playerCurrentScore;
+  players.player2.playerCurrentScore = 0;
+  document.getElementById('current--1').innerHTML = players.player2.playerCurrentScore;
+  document.getElementById('score--1').innerHTML = players.player2.playerScore;
+  players.player1.player1Active=true;
+  players.player2.player2Active=false;
+}
+  if(Number(players.player1.playerScore) >= Number(10))
+    document.querySelector('.player--0').classList.add('player--winner');
+  if(Number(players.player2.playerScore) >= Number(10))
+    document.querySelector('.player--1').classList.add('player--winner');
+}
 
-
-
-
+const newGameStart=() =>{
+players.player1.playerScore = 0;
+players.player1.playerCurrentScore = 0;
+players.player2.playerScore = 0;
+players.player2.playerCurrentScore = 0;
+document.querySelector('.player--0'). classList.remove('player--active');
+document.querySelector('.player--1').classList.remove('player--active');
+document.getElementById('score--0').innerHTML = players.player1.playerScore;
+document.getElementById('current--0').innerHTML = players.player1.playerCurrentScore;
+document.getElementById('score--1').innerHTML = players.player2.playerScore;
+document.getElementById('current--1').innerHTML = players.player2.playerCurrentScore;
+}
